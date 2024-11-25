b9d592df34ecb5828c6db6072de9d69f94806fc7
Sumit Dey <sumitdeyodisha@gmail.com>
2024-11-25 18:14:52 +0530

    Added the custom user model and admin fiels

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+from .forms import UserChangeForm, UserCreationForm
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    list_display = [
+        "email",
+        "role",
+        "phone",
+        "is_staff",
+        "is_superuser",
+        "is_active",
+        "last_login",
+    ]
+    list_filter = ["is_staff"]
+    fieldsets = [
+        (None, {"fields": ["email", "password"]}),
+        ("Permissions", {"fields": ["is_staff", "is_superuser"]}),
+    ]
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["email", "password1", "password2"],
+            },
+        ),
+    ]
+    search_fields = ["email"]
+    ordering = ["email"]
+    filter_horizontal = []
+
+
+admin.site.register(User, UserAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,60 @@
+from phonenumber_field.modelfields import PhoneNumberField
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+
+class Role(models.TextChoices):
+    ADMIN = "admin", "Admin"
+    USER = "user", "User"
+    MODERATOR = "moderator", "Moderator"
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("User must have an email address")
+        if not password:
+            raise ValueError("User must have a password")
+
+        user_obj = self.model(email=self.normalize_email(email), **extra_fields)
+
+        user_obj.set_password(password)
+        user_obj.save(using=self._db)
+        return user_obj
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    phone = PhoneNumberField(max_length=13, region="IN")
+    role = models.CharField(max_length=50, choices=Role.choices, default=Role.USER)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    # last_login = models.DateTimeField(auto_now=True) no need to add as it is already present by default
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELD = ["phone"]  # email and password are required by default
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, object=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
