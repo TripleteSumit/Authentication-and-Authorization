235a0c0476ebed011010a5a9833bc328873dbc8a
Sumit Dey <sumitdeyodisha@gmail.com>
2024-11-27 23:10:51 +0530

    Created the comment model

--- message/models.py
+++ message/models.py
@@ -4,7 +4,10 @@ from core.models import User
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comment")
-    message = models.CharField(max_length=255, blank=True)
+    message = models.CharField(
+        max_length=255,
+        blank=True,
+    )
     attachment = models.FileField(upload_to="comment_attachment", null=True, blank=True)
     archived = models.BooleanField(default=False)  # use for soft delete
     created_at = models.DateTimeField(auto_now_add=True)
