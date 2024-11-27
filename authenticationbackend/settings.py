e800dd36a2dc0884e80016e7254f72f107b98c50
Sumit Dey <sumitdeyodisha@gmail.com>
2024-11-27 21:16:59 +0530

    Added the comment model and set up the media upload files

--- authenticationbackend/settings.py
+++ authenticationbackend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from datetime import timedelta
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+
+MEDAI_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- message/models.py
+++ message/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from core.models import User
 
-# Create your models here.
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comment")
+    message = models.CharField(max_length=255, blank=True)
+    attachment = models.FileField(upload_to="comment_attachment", null=True, blank=True)
+    archived = models.BooleanField(default=False)  # use for soft delete
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
