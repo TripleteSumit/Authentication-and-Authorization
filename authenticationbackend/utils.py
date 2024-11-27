cc162ca34d4a9238e0ad54f22a0b9a858b1a815f
Sumit Dey <sumitdeyodisha@gmail.com>
2024-11-27 23:10:09 +0530

    update the validate incoming data method for patch method

--- authenticationbackend/utils.py
+++ authenticationbackend/utils.py
@@ -2,14 +2,18 @@ from typing import Dict, List
 
 
 def validate_incoming_data(
-    data: Dict[str, any], required_keys: List[str], allowed_keys: List[str] = []
+    data: Dict[str, any],
+    required_keys: List[str],
+    allowed_keys: List[str] = [],
+    partial: bool = False,
 ) -> List[Dict[str, str]]:
 
     errors: List[Dict[str, str]] = []
 
-    for key in required_keys:
-        if key not in data:
-            errors.append({key: "This is a required key."})
+    if not partial:
+        for key in required_keys:
+            if key not in data:
+                errors.append({key: "This is a required key."})
 
     for key in data:
         if key not in required_keys + allowed_keys:
